0700dbfd054fa88b1fa83b2c8c80fee8e29cbfb7
Charpell <charpellumeh@gmail.com>
2018-05-14 14:01:47 +0100

    Select news by country dynamically

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # News-App
 A react-native app for displaying current news all over the world
+
+[img.png]('./img.png')
--- src/news.js
+++ src/news.js
@@ -2,7 +2,11 @@
 //   .then(response => response.json())
 //   .then((news) => console.log(news))
 
-const URL = 'https://newsapi.org/v2/top-headlines?country=us&apiKey=a460cd20e3a14b8790ebf514344c57f5';
+// Countries = 'ae ar at au be bg br ca ch cn co cu cz de eg fr gb gr hk hu id ie il it jp kr lt lv ma mx my ng nl no nz ph pl pt ro rs ru sa se sg si sk th tr tw ua us ve za';
+
+const country = 'us'
+
+const URL = `https://newsapi.org/v2/top-headlines?country=${country}&apiKey=a460cd20e3a14b8790ebf514344c57f5`;
 
 export async function getNews() {
   let result = await fetch(URL).then(response => response.json())
